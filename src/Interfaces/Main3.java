ad813249cce6438d4baae9128f0b13ed3b75f1a6
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2024-06-17 15:20:27 -0300

    race condition com semaforo

---
+++ src/Interfaces/Main3.java
@@ -0,0 +1,66 @@
+package Interfaces;
+import java.util.concurrent.Semaphore;
+
+public class Main3 {
+
+    public static class Semaphore {
+        private int permit;
+
+        public Semaphore(int permit) {
+            this.permit = permit;
+        }
+
+        public synchronized void take() throws InterruptedException {
+            while (permit <= 0) {
+                wait();
+            }
+            permit--;
+        }
+
+        public synchronized void release() throws InterruptedException{
+            permit ++;
+            notify();
+        }
+    }
+
+    public static class Fio implements Runnable{
+        public Semaphore s;
+        public String palavra;
+
+        public Fio(Semaphore s, String palavra) {
+            this.s = s;
+            this.palavra = palavra;
+        }
+
+        public void run() {
+            System.out.println(palavra+ " Tentando acessar...");
+            System.out.println("...");
+            try {
+                s.take();
+            }catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Palavra: "+palavra);
+
+            try{
+                Thread.sleep(3000);
+                s.release();
+            }catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Semaphore semaforo = new Semaphore(1);
+
+        Fio Fio1 = new Fio(semaforo, "A");
+        Thread Fio1Thread = new Thread(Fio1);
+
+        Fio Fio2 = new Fio(semaforo, "B");
+        Thread Fio2Thread = new Thread(Fio2);
+
+        Fio1Thread.start();
+        Fio2Thread.start();
+    }
+}
